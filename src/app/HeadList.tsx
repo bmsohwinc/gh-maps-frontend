0277d4aef2ce0b6bf0e58bfb022500c4dc79d4f8
Sourabha Bharadwaj BM <bharasou58@gmail.com>
2024-05-03 21:34:10 +0530

    Refactor font

--- src/app/HeadList.tsx
+++ src/app/HeadList.tsx
@@ -1,12 +1,14 @@
+import { Typography } from "@mui/material";
+
 export default function HeadList(props) {
     return (
         <div className="headlist">
             <div className="headlist-head">
-                {props.header}
+                <Typography variant="h4">{props.header}</Typography>
             </div>
             <div className="headlist-children">
                 {props.children}
             </div>
         </div>
-    );    
+    );
 }
