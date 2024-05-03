bb9a20858850b36d9066458c73dd83277bab4891
Sourabha Bharadwaj BM <bharasou58@gmail.com>
2024-05-03 20:24:00 +0530

    Change coder card design

--- src/app/lists/Coder.tsx
+++ src/app/lists/Coder.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
 import Avatar from '@mui/material/Avatar';
 import { Item } from "./Item";
 import { numberFormatter } from "../utils";
@@ -7,34 +7,53 @@ import { numberFormatter } from "../utils";
 export default function Coder(props) {
     // login, name, avatar, followers
     return (
-        <Item onClick={() => props.onClick({login: props.login, avatarUrl: props.avatarUrl})}>
+        <Item
+            onClick={() => props.onClick({ login: props.login, avatarUrl: props.avatarUrl })}
+            sx={{
+                width: '97%',
+            }}
+        >
             <Grid item xs={12} container spacing={2} height='100%'>
-                <Grid item xs={8} height='100%'>
-                    <Grid item xs={12} container spacing={2} height='100%'>
-                        <Grid item xs={2} height='100%'>
-                            <a
-                                href={`https://github.com/${props.login}`} 
-                                target="_blank"
-                            >
-                                <Avatar alt={props.login} src={props.avatarUrl} />
-                            </a>
-                        </Grid>
-                        <Grid item xs={5} height='100%'>
-                            <a
-                                href={`https://github.com/${props.login}`} 
-                                target="_blank"
-                            >
-                                <h3 style={{paddingTop: 7}}>@{props.login}</h3>
-                            </a>
-                        </Grid>
-                        <Grid item xs={5} height='100%'>
-                            <h3>{props.name}</h3>
-                        </Grid>
-                    </Grid>
-                    
+                <Grid item xs={7} height='100%'>
+                    <Stack
+                        direction="row"
+                        alignItems="center"
+                        gap={2}
+                    >
+                        <a href={`https://github.com/${props.login}`} target="_blank">
+                            <Avatar alt={props.login} src={props.avatarUrl} sx={{ width: 48, height: 48 }} />
+                        </a>
+                        <div style={{
+                            textAlign: 'left',
+                        }}>
+                            <Typography variant="h6">
+                                <a href={`https://github.com/${props.login}`} target="_blank">
+                                    @{props.login}
+                                </a>
+                            </Typography>
+                            {props.name ?? <div>&middot;</div>}
+                        </div>
+                    </Stack>
                 </Grid>
-                <Grid item xs={4} height='100%'>
-                    <h3>{numberFormatter.format(props.followers)} Followers</h3>
+                <Grid item xs={5} height='100%'>
+                    <Stack
+                        direction="row"
+                        alignItems="center"
+                        gap={2}
+                    >
+                        <div>
+                            <Typography variant="h6">
+                                {numberFormatter.format(props.followings)}
+                            </Typography>
+                            Followings
+                        </div>
+                        <div>
+                            <Typography variant="h6">
+                                {numberFormatter.format(props.followers)}
+                            </Typography>
+                            Followers
+                        </div>
+                    </Stack>
                 </Grid>
             </Grid>
         </Item>
